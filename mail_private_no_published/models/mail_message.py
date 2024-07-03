fcbc0a20d4ccf34b83055661c4615a30e5ac6576
Valentín Georgian Castravete <vgeorgian@sidoo.es>
2024-07-03 12:14:15 +0200

    [IMP] Make notes also non website published

--- mail_private_no_published/models/mail_message.py
+++ mail_private_no_published/models/mail_message.py
@@ -38,6 +38,10 @@ class MailMessage(models.Model):
     def create(self, vals):
         res = super().create(vals)
         for rec in res:
+            if rec.subtype_id == self.env.ref('mail.mt_note'):
+                rec.website_published = False
+                continue
+
             partner_ids = rec.partner_ids.ids
             user_ids = self.env['res.users'].search([
                 ('partner_id', 'in', partner_ids),
